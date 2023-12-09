4591ca3d34a81a6698e8b14931c515adf4da051e
unknown <freelancernayem.me@gmail.comm>
2023-12-09 14:18:06 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -25,33 +28,182 @@ async function run() {
     await client.connect();
     const menuCollection = client.db("bistroDB").collection("menu");
     const cartCollection = client.db("bistroDB").collection("cart");
-    
-    app.get('/menu',async(req,res)=>{
-        const result = await menuCollection.find().toArray()
-        res.send(result)
+    const userCollection = client.db("bistroDB").collection("users");
+    const paymentCollection = client.db("bistroDB").collection("payments");
+
+
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray()
+      res.send(result)
     })
 
     // add to cart 
-    app.post('/carts',async(req,res)=>{
+    app.post('/carts', async (req, res) => {
       const cartitem = req.body;
       const result = await cartCollection.insertOne(cartitem);
       res.send(result)
     })
 
-    app.get('/carts',async(req,res)=>{
-       const email = req.query.email;
-       const query = {email : email}
-        const result = await cartCollection.find(query).toArray()
-        res.send(result)
-    }) 
-    
-    app.delete('/carts/:id',async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await cartCollection.deleteOne(query) 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray()
       res.send(result)
-      
     })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+
+    })
+    // jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.Access_Token, {
+        expiresIn: '1h',
+      })
+      res.send({ token })
+    })
+    // midlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).sed({ meassage: "unauthorization" })
+
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.Access_Token, (err, decoded) => {
+        if (err) {
+          return res.status(401).sed({ meassage: "unauthorization" })
+
+        }
+        req.decoded = decoded;
+
+        next()
+
+      })
+
+    }
+
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).sed({ meassage: "forbidden access" })
+
+      }
+      next();
+    }
+
+
+
+    // users
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // email unique
+      const query = { email: user?.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
+
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+
+    })
+    app.patch('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const update = {
+        $set: {
+          role: 'admin',
+        }
+      }
+      const result = await userCollection.updateOne(filter, update);
+      res.send(result)
+
+
+    })
+    // add items
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const items = req.body;
+      const result = await menuCollection.insertOne(items);
+      res.send(result)
+    })
+
+    // paymeny getway
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"]
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+
+
+    })
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+
+    })
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
